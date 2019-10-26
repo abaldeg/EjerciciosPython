922cb33a3203f74ba943051eefdd150209c69419
abaldeg <abaldeg@gmail.com>
2019-10-26 17:14:15 -0300

    Create tp 6 ej 15.py

---
+++ tp 6 ej 15.py
@@ -0,0 +1,16 @@
+# """ Ejercicio 15: 
+# Leer dos listas de números M y N, ambas ordenadas de menor a mayor. Generar 
+# e imprimir una tercera lista que resulte de intercalar los elementos de M y N. La 
+# nueva lista también debe quedar ordenada, sin utilizar ningún método de orde- 
+# namiento. 
+
+M=[1,2,3,4,5]
+N=[100,200,300,400,500]
+l=[]
+
+largo=len(M)
+for i in range(largo):
+    l[i]=M[i]
+    l[i]=N[i]
+
+
