5f8932b058651c48c62b7b16c728dd0fa8b4d30c
abaldeg <abaldeg@gmail.com>
2020-03-21 17:12:27 -0300

    Update PROGRAMACION I TP 1 EJ 1.py

--- PROGRAMACION I TP 1 EJ 1.py
+++ PROGRAMACION I TP 1 EJ 1.py
@@ -3,13 +3,19 @@
 def ObtenerMayor(n1,n2,n3):
     if n1 > n2:
         if n1 > n3:
-            return n1        
-    elif n2 > n3:
+            return n1
+        else:
+            return -1
+    if n2 > n3:
+        if n2 > n1:
             return n2
-    elif n3 > n1:
-        return n3
-    else:
-        return -1
+        else:
+            return -1
+    if n3 > n1:
+        if n3 > n2:
+            return n3
+        else:
+            return -1
         
 # Programa Principal    
 n1=int(input("Ingrese N1:"))
@@ -17,6 +23,6 @@ n2=int(input("Ingrese N2:"))
 n3=int(input("Ingrese N3:"))
 r=ObtenerMayor(n1,n2,n3)
 if r==-1:
-    print("No hay mayor")
+    print("No hay mayor absoluto")
 else:
-    print ("El número mayor es: ",r)
+    print ("El número mayor absoluto es: ",r)
