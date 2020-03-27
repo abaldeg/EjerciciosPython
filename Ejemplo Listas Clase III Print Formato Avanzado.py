2ed92ac582a4b35e8b9a6dae40521117a7b0b238
abaldeg <abaldeg@gmail.com>
2020-03-27 08:10:58 -0300

    Create Ejemplo Listas Clase III Print Formato Avanzado.py

---
+++ Ejemplo Listas Clase III Print Formato Avanzado.py
@@ -0,0 +1,17 @@
+votos = [ ]
+n = int(input("Votos del candidato? (-1 para terminar): "))
+while n !=-1:
+    votos = votos + [n] # votos.append(n) sería similar
+    n = int(input("Votos del candidato? (-1 para terminar) "))
+
+print( ) 
+
+# Calcular porcentajes e imprimir listado final 
+total = sum(votos)
+for i in range(len(votos)):
+    porcentaje = votos[i] * 100 / total
+    print("• Candidato %d: %d votos (%.2f%%)" %(i, votos[i], porcentaje), end=" ")
+    # Imprimir el gráfico de barras
+    for j in range(int(porcentaje/10)):
+        print("*", end="")
+    print( )
