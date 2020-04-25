e0704ac2be8a538f1b8aaaaa723120c0d2c960ab
abaldeg <abaldeg@gmail.com>
2020-04-25 18:48:43 -0300

    ejercicios antes del parcial

--- PROGRAMACION I TP 2 EJ 3.py
+++ PROGRAMACION I TP 2 EJ 3.py
@@ -1,3 +1,21 @@
 #Crear una lista con los cuadrados de los números entre 1 y N (ambos incluidos),
 #donde N se ingresa desde el teclado. Luego se solicita imprimir los últimos 10 valores
 #de la lista.
+
+#Funciones
+def crearListaCuadrados(l,n):
+    l=[0]*(n+1)
+    for i in range(1,n+1):
+        l[i]=i**2
+    return (l)
+    
+def imprimirLista(l):
+    l.sort(reverse=True)
+    for i in range(1, 11):
+        print(l[i])    
+
+#Programa Principal
+l=[]
+n=int(input("Ingrese Ultimo valor de la lista: "))
+l=crearListaCuadrados(l,n)
+imprimirLista(l)
