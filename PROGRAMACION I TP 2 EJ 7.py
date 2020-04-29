d20fc9caed7e6b12e0c434a5df9a7b2a0caf68a1
abaldeg <abaldeg@gmail.com>
2020-04-28 21:12:47 -0300

    Create PROGRAMACION I TP 2 EJ 7.py

---
+++ PROGRAMACION I TP 2 EJ 7.py
@@ -0,0 +1,26 @@
+#Escribir una función que reciba una lista como parámetro y devuelva True si la lista
+#está ordenada en forma ascendente o False en caso contrario. Por ejemplo,
+#ordenada([1, 2, 3]) retorna True y ordenada(['b', 'a']) retorna False. Desarrollar
+#además un programa para verificar el comportamiento de la función.
+
+#Funciones
+def analizaOrdenLista(lista):
+    ordenada=True
+    if len(lista) <= 1:
+        #
+        print ("La lista esta vacía o tiene un solo elemento")
+        ordenada=False
+    else:
+        for i in range(len(lista)-1):
+            if lista[i]>lista[i+1]:
+                ordenada=False    
+    return(ordenada)
+
+
+#Programa Principal
+lista=[1,2,3]
+print(analizaOrdenLista(lista))
+lista=[2,1,3]
+print(analizaOrdenLista(lista))
+lista=[2]
+print(analizaOrdenLista(lista))
