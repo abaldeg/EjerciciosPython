9eacf9628c1b8645baba593456a08b3949130cf3
abaldeg <abaldeg@gmail.com>
2020-05-20 11:01:06 -0300

    Create PROGRAMACION I TP 4 EJ 10.py

---
+++ PROGRAMACION I TP 4 EJ 10.py
@@ -0,0 +1,3 @@
+# Escribir un programa que permita ingresar una cadena de caracteres e imprima un
+# mensaje indicando cuántas letras y cuántos números contiene. Por ejemplo, si se
+# ingresa "Hola Mundo 123" debe indicar que se ingresaron 9 letras y 3 números
