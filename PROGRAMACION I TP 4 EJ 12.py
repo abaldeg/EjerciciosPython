1603c1cd0c48ade58af527b1f7fc6fe8babe21f7
abaldeg <abaldeg@gmail.com>
2020-05-20 12:22:31 -0300

    Create PROGRAMACION I TP 4 EJ 12.py

---
+++ PROGRAMACION I TP 4 EJ 12.py
@@ -0,0 +1,4 @@
+#Desarrollar una función para reemplazar todas las apariciones de una palabra por
+#otra en una cadena de caracteres y devolver la cadena obtenida y un entero con la
+#cantidad de reemplazos realizados. Escribir también un programa para verificar el
+#comportamiento de la función
