472a276ed92ef940385b2a74ae92b917068cde4f
abaldeg <abaldeg@gmail.com>
2020-05-24 08:44:26 -0300

    nuevos ejercicios tp 5

---
+++ PROGRAMACION I TP 4 EJ 15.py
@@ -0,0 +1,11 @@
+#Muchas aplicaciones financieras requieren que los números sean expresados también
+#en letras. Por ejemplo, el número 2153 puede escribirse como "dos mil ciento
+#cincuenta y tres". Escribir un programa que utilice una función para convertir un
+#número entero entre 0 y 1 billón (1.000.000.000.000) a letras. ¿En qué cambiaría
+#la función si también aceptara números negativos? ¿Y números con decimales?
+unidades=["uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve"]
+decenas=["diez", "veinte", "treinta", "cuarenta", "cincuenta", "sesenta", "setenta", "ochenta", "noventa"]
+centenas=["cien", "doscientos", "trescientos", "cuatrocientos", "quinientos", "seiscientos", "setecientos", "ochocientos", "novecientos"]
+miles=["mil", "dos mil","tres mil","cuatro mil","cinco mil","seis mil","siete mil","ocho mil","nueve mil"]
+decenamiles=["diez mil","veinte mil","treinta mil","cuarenta mil","cincuenta mil","sesenta mil","setenta mil","ochenta mil", "noventa mil"]
+cientosmiles=["cien mil","doscientos mil","trescientos mil","cuatrocientos mil","quinientos mil","seiscientos mil","setecientos mil","ochociento mil", "novecientos mil"]
---
+++ PROGRAMACION I TP 5 EJ 2.py
@@ -0,0 +1,7 @@
+#Realizar una función que reciba como parámetros dos cadenas de caracteres conteniendo
+#números reales, sume ambos valores y devuelva el resultado como un
+#número real. Devolver -1 si alguna de las cadenas no contiene un número válido,
+#utilizando manejo de excepciones para detectar el error
+
+# NO SE COMO HACER PARA QUE DE ERROR UNA SUMA DE NUMEROS REALES.
+
