a5532cb09cc1a35b3ea1b862f2edbb4b9d48f459
abaldeg <abaldeg@gmail.com>
2020-05-24 12:42:42 -0300

    tp 6

---
+++ PROGRAMACION I TP 6 EJ 1.py
@@ -0,0 +1,27 @@
+#Desarrollar un programa para eliminar todos los comentarios de un programa escrito
+#en lenguaje Python. Tener en cuenta que los comentarios comienzan con el
+#signo # (siempre que éste no se encuentre encerrado entre comillas simples o dobles)
+#y que también se considera comentario a las cadenas de documentación (docstrings).
+#C:\Users\abald\OneDrive\Documentos\GitHub\EjerciciosPython\tp_archivos\tp 6 ej 3.py
+
+try:
+    archentrada=open(r"C:\Users\abald\OneDrive\Documentos\GitHub\EjerciciosPython\tp_archivos\tp 6 ej 3.py",rt)
+    archsalida=open(r"C:\Users\abald\OneDrive\Documentos\GitHub\EjerciciosPython\tp_archivos\tp 6 ej 3_sinComentarios.py",rt)
+    cantlineas=0
+    for linea in archentrada:
+        linea=linea.rstrip(r"\n#""")
+        archsalida.write(linea)
+        cantlineas+=1
+except FileNotFound as mensaje:
+    print("No se puede abrir el archivo: ", mensaje)
+except OSError as mensaje:
+    print("Error: ", mensaje)
+else:
+    print("Copia Finalizada")
+    
+
+    
+        
+        
+            
+            
---
+++ tp_archivos/tp 6 ej 3.py
@@ -0,0 +1,35 @@
+# En los siguientes ejercicios utilice la función del ejercicio I para ingresar datos en una lista y:
+# Determinar Si una lista es capicúa.
+# 4554 45454
+def CrearLista ():
+    l = []
+    pos=0
+    n=int(input("Ingrese un número (-1 para finalizar)"))    
+    while n != -1:        
+        while (n < 1 and n != -1) or n > 20:
+            n=int(input("Error. Ingrese un número entre 1 y 20 (-1 para finalizar)"))
+        if n != -1:
+            l.append(n)
+            pos=pos+1
+            n=int(input("Ingrese un número (-1 para finalizar)"))    
+    return l
+
+l=CrearLista()
+largo=len(l)
+capicua=False
+derecha = 0
+if largo % 2 !=0:
+    for i in range(largo):
+        if l[i]==l[(largo-1)-derecha]:
+            capicua=True
+        else:
+            capicua=False
+        derecha=derecha+1
+else: #para tp de archivos. esta linea tiene que quedar eliminada.
+    print("La cantidad de elementos de la lista debe ser impar")
+
+if capicua==True:
+    print("La lista es capicua")
+else:
+    print("La lista NO es capicua")
+    
